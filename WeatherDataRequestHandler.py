b7af545ec13fb88448b32ddfe42ee7fe1a63f25e
JeffreyCoxChatham <jeffrey.cox@chatham.edu>
2025-04-16 14:20:23 -0400

    Implemented error handling
    Implemented error handling, so the server shouldn't crash given a wrong request. Also standardized some single and double quotes.

--- WeatherDataRequestHandler.py
+++ WeatherDataRequestHandler.py
@@ -10,19 +10,33 @@ class WeatherDataRequestHandler(BaseHTTPRequestHandler):
         # /records/latest/data.json -> records with latest
         # /predictions/latest/data.json -> predictions with latest
         # /latest/data.json -> records with latest
-        if self.path.lower().startswith("/records") or self.path.lower().startswith("/predictions"):
-            sheet = self.path.split("/")[1]
-            time = self.path.split("/")[2]
-            response = self.do_entry(sheet, int(time))
+        if self.path.lower().startswith("/records/") or self.path.lower().startswith("/predictions/"):
+            sheet = self.path.lower().split("/")[1]
+            time = self.path.lower().split("/")[2]
+            if time == "latest":
+                response = self.do_entry(sheet, "latest")
+            else:
+                try:
+                    time = int(time)
+                except:
+                    response = "Invalid query: provided timestamp must be an integer or 'latest'!"
+                else:
+                    response = self.do_entry(sheet, time)
         elif self.path.lower().startswith("/latest"):
-            response = self.do_entry('records', 'latest')
+            response = self.do_entry("records", "latest")
         else:
-            response = 'i am error'
+            response = "Invalid query: malformed path!"
         
-        self.send_response(200)
-        self.send_header('Content-type', 'text/json')
-        self.end_headers()
-        self.wfile.write(response.encode('utf-8'))
+        if response == "Invalid query: provided timestamp must be an integer!" or response == "Invalid query: malformed path!" or response == "Invalid query: SQL error!":
+            self.send_response(400)
+            self.send_header("Content-type", "text")
+            self.end_headers()
+            self.wfile.write(response.encode("utf-8"))
+        else:
+            self.send_response(200)
+            self.send_header("Content-type", "text/json")
+            self.end_headers()
+            self.wfile.write(response.encode("utf-8"))
     
     
     # get a specified entry based on a timestamp and a sheet. returns a json dump
@@ -30,22 +44,25 @@ class WeatherDataRequestHandler(BaseHTTPRequestHandler):
         con = sqlite3.connect("sample-db.db")
         cur = con.cursor()
         
-        if time == 'latest':
-            entry = cur.execute(f"""
-                SELECT ID, Time, Pm1p0, Pm2p5, Pm4p0, Pm10p0, Humidity, Temp, VocIndex, NoxIndex, CO2
-                FROM {sheet}
-                ORDER BY Time DESC
-                LIMIT 1
-            """).__next__()
-        else:
-            entry = cur.execute(f"""
-                SELECT ID, Time, Pm1p0, Pm2p5, Pm4p0, Pm10p0, Humidity, Temp, VocIndex, NoxIndex, CO2
-                FROM {sheet}
-                WHERE Time={time}
-            """).__next__()
+        try:
+            if time == "latest":
+                entry = cur.execute(f"""
+                    SELECT ID, Time, Pm1p0, Pm2p5, Pm4p0, Pm10p0, Humidity, Temp, VocIndex, NoxIndex, CO2
+                    FROM {sheet}
+                    ORDER BY Time DESC
+                    LIMIT 1
+                """).__next__()
+            else:
+                entry = cur.execute(f"""
+                    SELECT ID, Time, Pm1p0, Pm2p5, Pm4p0, Pm10p0, Humidity, Temp, VocIndex, NoxIndex, CO2
+                    FROM {sheet}
+                    WHERE Time={time}
+                """).__next__()
+        except:
+            return "Invalid query: SQL error!"
         
         entryDict = {}
-        dictLabels = ('ID', 'Time', 'Pm1p0', 'Pm2p5', 'Pm4p0', 'Pm10p0', 'Humidity', 'Temp', 'VocIndex', 'NoxIndex', 'CO2')
+        dictLabels = ("ID", "Time", "Pm1p0", "Pm2p5", "Pm4p0", "Pm10p0", "Humidity", "Temp", "VocIndex", "NoxIndex", "CO2")
         for i in range(len(entry)):
             entryDict[dictLabels[i]] = str(entry[i])
         entryJson = json.dumps(entryDict, indent=4)
