23ce2876bdbf076684d13dd189469ed440bd11ad
JeffreyCoxChatham <jeffrey.cox@chatham.edu>
2025-04-23 16:28:46 -0400

    Updated formatting
    This was all done because sam made some changes with the formatting that weren't communicated; additionally, the sheet has no predictions table. Adding prediction requests back in would be easy though, see previous revisions.

--- WeatherDataRequestHandler.py
+++ WeatherDataRequestHandler.py
@@ -5,25 +5,23 @@ from http.server import BaseHTTPRequestHandler
 class WeatherDataRequestHandler(BaseHTTPRequestHandler):
     
     def do_GET(self):
-        # /records/173856239 -> records with timestamp
-        # /predictions/57363464356 -> predictions with timestamp
+        # /records/173856239.6860485 -> records with timestamp
         # /records/latest -> records with latest
-        # /predictions/latest -> predictions with latest
         # /latest -> records with latest
-        if self.path.lower().startswith("/records/") or self.path.lower().startswith("/predictions/"):
+        if self.path.lower().startswith("/airQualityReadings/"):
             sheet = self.path.lower().split("/")[1]
             time = self.path.lower().split("/")[2]
             if time == "latest":
                 response = self.getEntry(sheet, "latest")
             else:
                 try:
-                    time = int(time)
+                    time = float(time)
                 except:
-                    response = ["Invalid query: provided timestamp must be an integer or 'latest'!", "error"]
+                    response = ["Invalid query: provided timestamp must be a float or 'latest'!", "error"]
                 else:
                     response = self.getEntry(sheet, time)
         elif self.path.lower().startswith("/latest"):
-            response = self.getEntry("records", "latest")
+            response = self.getEntry("airQualityReadings", "latest")
         else:
             response = ["Invalid query: malformed path!", "error"]
         
@@ -56,22 +54,22 @@ class WeatherDataRequestHandler(BaseHTTPRequestHandler):
         try:
             if time == "latest":
                 entry = cur.execute(f"""
-                    SELECT ID, Time, Pm1p0, Pm2p5, Pm4p0, Pm10p0, Humidity, Temp, VocIndex, NoxIndex, CO2
+                    SELECT readingsID, timeStamp, Pm1p0, Pm2p5, Pm4p0, Pm10p0, Humidity, Temp, VocIndex, NoxIndex, CO2
                     FROM {sheet}
-                    ORDER BY Time DESC
+                    ORDER BY timeStamp DESC
                     LIMIT 1
                 """).__next__()
             else:
                 entry = cur.execute(f"""
-                    SELECT ID, Time, Pm1p0, Pm2p5, Pm4p0, Pm10p0, Humidity, Temp, VocIndex, NoxIndex, CO2
+                    SELECT readingsID, timeStamp, Pm1p0, Pm2p5, Pm4p0, Pm10p0, Humidity, Temp, VocIndex, NoxIndex, CO2
                     FROM {sheet}
-                    WHERE Time={time}
+                    WHERE timeStamp={time}
                 """).__next__()
         except:
             return ["Invalid query: SQL error!", "error"]
         
         entryDict = {}
-        dictLabels = ("ID", "Time", "Pm1p0", "Pm2p5", "Pm4p0", "Pm10p0", "Humidity", "Temp", "VocIndex", "NoxIndex", "CO2")
+        dictLabels = ("readingsID", "timeStamp", "Pm1p0", "Pm2p5", "Pm4p0", "Pm10p0", "Humidity", "Temp", "VocIndex", "NoxIndex", "CO2")
         for i in range(len(entry)):
             entryDict[dictLabels[i]] = str(entry[i])
         entryDict["Sheet"] = sheet
